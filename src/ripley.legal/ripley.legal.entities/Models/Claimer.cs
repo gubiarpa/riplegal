1b3bbb1d4f0d4854f3f4c94193da12ccb8e80849
gubiarpa <billy.arredondo@pucp.pe>
2020-09-07 01:36:31 -0500

    Claim Create
    Combobox loading

--- src/ripley.legal/ripley.legal.entities/Models/Claimer.cs
+++ src/ripley.legal/ripley.legal.entities/Models/Claimer.cs
@@ -1,12 +1,14 @@
 ﻿using ripley.legal.entities.Models.Behavior;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace ripley.legal.entities.Models
 {
     public class Claimer : GuidDescBase
     {
-        public IGuid DocumentType { get; set; }
+        [Column("IDDocumentType")]
+        public Guid DocumentType { get; set; }
     }
 }
--- src/ripley.legal/ripley.legal.web/Controllers/ClaimsController.cs
+++ src/ripley.legal/ripley.legal.web/Controllers/ClaimsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Cache;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -30,14 +31,63 @@ namespace ripley.legal.web.Controllers
         #region AjaxRequests
         public async Task<IActionResult> GetLawFirms()
         {
-            var lawFirms = await _context.LawFirms.ToListAsync();
+            var lawFirms = await _context.LawFirms.OrderBy(x => x.Description).ToListAsync();
             return new JsonResult(lawFirms);
+        }
+
+        public async Task<IActionResult> GetAgencies()
+        {
+            try
+            {
+                var agencies = await _context.Agencies.OrderBy(x => x.Description).ToListAsync();
+                return new JsonResult(agencies);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public async Task<IActionResult> GetCities()
         {
-            var cities = await _context.Cities.ToListAsync();
+            var cities = await _context.Cities.OrderBy(x => x.Description).ToListAsync();
             return new JsonResult(cities);
+        }
+
+        public async Task<IActionResult> GetProcessTypes()
+        {
+            var processTypes = await _context.ProcessTypes.ToListAsync();
+            return new JsonResult(processTypes);
+        }
+
+        public async Task<IActionResult> GetClaimers()
+        {
+            var claimers = await _context.Claimers.ToListAsync();
+            return new JsonResult(claimers);
+        }
+
+        public async Task<IActionResult> GetDefendants()
+        {
+            var defendants = await _context.Defendants.ToListAsync();
+            return new JsonResult(defendants);
+        }
+
+        public async Task<IActionResult> GetTypologies()
+        {
+            var typologies = await _context.Typologies.ToListAsync();
+            return new JsonResult(typologies);
+        }
+
+        public async Task<IActionResult> GetLegalPositions()
+        {
+            var legalPositions = await _context.LegalPositions.ToListAsync();
+            return new JsonResult(legalPositions);
+        }
+
+        public async Task<IActionResult> GetDefenseAssessments()
+        {
+            var defenseAssessments = await _context.DefenseAssessments.ToListAsync();
+            return new JsonResult(defenseAssessments);
         }
         #endregion
     }
